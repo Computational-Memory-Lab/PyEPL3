35c8ae771a8eafda062897e1076e768f1830435a
kapawluk <kpawluk@ualberta.ca>
2026-01-18 23:34:17 -0700

    Update config_pairassoc.py
    Default number of test lists is 15 now.

--- config_pairassoc.py
+++ config_pairassoc.py
@@ -7,7 +7,7 @@ This config file sets parameters for the paired associate memory experiment.
 from pyepl3.pyepl3 import Key
 
 # Experiment structure
-NLISTS = 12  # Number of study-test lists
+NLISTS = 15  # Number of study-test lists
 RUN_PRACTICE = 2  # Whether to run practice list-pair (2) or not (0)
 NPAIRS = 16  # Number of word pairs per list
 NPAIRS_PRACTICE = 8 # Number of word pairs per practice list
